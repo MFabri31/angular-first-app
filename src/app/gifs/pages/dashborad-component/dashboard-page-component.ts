3db36e5d19d22b8ccb305dbf9b1a98b912ed084c
mfabri31 <fabriciomedina9531@gmail.com>
2026-04-01 18:15:47 +0200

    add side menu components and update imports in dashboard page

--- src/app/gifs/pages/dashborad-component/dashboard-page-component.ts
+++ src/app/gifs/pages/dashborad-component/dashboard-page-component.ts
@@ -1,8 +1,11 @@
 import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { SideMenuHeader } from '../../components/side-menu-header/side-menu-header.component';
+import { SideMenuOptions } from '../../components/side-menu-options/side-menu-options.component';
 
 @Component({
   selector: 'dashboard-page-component',
-  imports: [],
+  imports: [RouterOutlet, SideMenuHeader, SideMenuOptions],
   templateUrl: './dashboard-page-component.html',
 })
 export default class DashboardPageComponent {
